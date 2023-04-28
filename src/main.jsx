4275d0f02df60130a22173e6c67e2ccbdaaa803f
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-28 19:25:21 -0300

    cambios generales

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import { PortafolioApp } from './PortafolioApp';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <PortafolioApp />
+    </BrowserRouter>
+  </React.StrictMode>
+);
