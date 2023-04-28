f7a3ae6160c838cb681c8ca8e9100b2114ffcad3
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-28 19:22:53 -0300

    rutas y navbar

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { About } from '../portafolioSections/pages/About';
+import { Contact } from '../portafolioSections/pages/Contact';
+import { Home } from '../portafolioSections/pages/Home';
+import { Portfolio } from '../portafolioSections/pages/Portfolio';
+import { Navbar } from '../ui/components/Navbar';
+
+export const AppRouter = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="Home" element={<Home />}></Route>
+        <Route path="Portfolio" element={<Portfolio />}></Route>
+        <Route path="About" element={<About />}></Route>
+        <Route path="Contact" element={<Contact />}></Route>
+      </Routes>
+    </>
+  );
+};
