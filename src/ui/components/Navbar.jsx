e118aef5121f7892bd8bf58cd90c07e85ba8ac12
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-29 01:32:07 -0300

    se utiliza NavLink para cuando esta activa la ruta + hover del texto + primera maqueracion

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
-    <div className='bg-red-700'>Hola mundo!</div>
-  )
-}
+    <div className="p-2 bg-black text-cyan-600 font-baskerville">
+      <div className="flex justify-between">
+        <ul className="flex">
+          <li className="font-dancingScript text-3xl">Leolux</li>
+        </ul>
+        <div className="flex justify-end">
+          <NavLink
+            className={({ isActive }) =>
+              `text-cyan-600 hover:text-gray-400 py-2 mr-3 ${isActive ? 'border-b-4 border-cyan-800' : ''}`
+            }
+            to={'/'}>
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `text-cyan-600 hover:text-gray-400 py-2 mr-3 ${isActive ? 'border-b-4 border-cyan-800' : ''}`
+            }
+            to={'/portfolio'}>
+            Portfolio
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `text-cyan-600 hover:text-gray-400 py-2 mr-3 ${isActive ? 'border-b-4 border-cyan-800' : ''}`
+            }
+            to={'/Contact'}>
+            Contact
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `text-cyan-600 hover:text-gray-400 py-2 mr-3 ${isActive ? 'border-b-4 border-cyan-800' : ''}`
+            }
+            to={'/About'}>
+            About
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
