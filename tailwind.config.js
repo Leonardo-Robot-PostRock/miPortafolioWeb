e23ee37a71b40a1d9549b73a574f0fa48bc03f73
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-29 01:29:17 -0300

    se configuran las tipografias para el proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'dancingScript': ['Dancing Script', 'cursive'],
+      'baskerville': ['Libre Baskerville', 'serif'],
+      'victorMono': ['Victor Mono', 'sans-serif'],
+      'victorMonoItalic': ['Victor Mono Italic', 'sans-serif'],
+      'victorMonoBold': ['Victor Mono Bold', 'sans-serif']
+    }
   },
   plugins: [],
 }
