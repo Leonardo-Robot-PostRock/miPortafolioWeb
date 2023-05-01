6b38b885c2df1fdbabaa7ddcc1aa24517a79c400
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-30 22:45:32 -0300

    rutas de app router modificadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { About } from '../portafolioSections/pages/About';
-import { Contact } from '../portafolioSections/pages/Contact';
-import { Home } from '../portafolioSections/pages/Home';
-import { Portfolio } from '../portafolioSections/pages/Portfolio';
 import { Navbar } from '../ui/components/Navbar';
+import { Main } from '../portafolioSections/pages/Main';
 
 export const AppRouter = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="Portfolio" element={<Portfolio />}></Route>
-        <Route path="About" element={<About />}></Route>
-        <Route path="Contact" element={<Contact />}></Route>
+        <Route path="/" element={<Main />}></Route>
         {/* Provisorio */}
         <Route path="*" element={<p>Path not resolved</p>} />
       </Routes>
