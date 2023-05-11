ed1fa15a977855814ee7394ad306c4938de6d65c
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-10 22:04:11 -0300

    clases de modo oscuro

--- src/portafolioSections/section/About.jsx
+++ src/portafolioSections/section/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const About = () => {
   return (
-    <div className="h-screen" id="about">
+    <div className="h-screen dark:bg-slate-950 dark:text-slate-200" id="about">
       About
     </div>
   );
