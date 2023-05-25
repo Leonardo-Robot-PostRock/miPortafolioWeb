e5969f3dbf30bbffbf9bfd0118b192a433963d19
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-25 20:22:24 -0300

    se cambia diseño

--- src/portafolioSections/section/Home.jsx
+++ src/portafolioSections/section/Home.jsx
@@ -1,52 +1,68 @@
 import React from 'react';
 import foto from './../../assets/images/foto.png';
+import fotoWhite from './../../assets/images/foto_bgWhite.jpg';
 import { ReactComponent as Twitter } from '../../assets/icons/twitter.svg';
 import { ReactComponent as Github } from '../../assets/icons/github.svg';
 import { ReactComponent as Linkedin } from '../../assets/icons/linkedin.svg';
-import { PresentationWithEffects } from '../components/PresentationWithEffects';
 
-export const Home = () => {
+export const Home = ({ darkMode }) => {
   return (
     <main
-      className="flex min-w-280 flex-wrap place-content-center items-center bg-slate-100 font-victorMono dark:bg-gray-900 dark:text-slate-300 sm:h-max md:h-screen"
+      className="flex h-screen min-w-280 flex-col place-content-center items-center bg-slate-100 font-victorMono dark:bg-neutral-900 dark:text-slate-300 md:mx-28"
       id="home">
-      <section className="W-2/4 mt-10 items-end md:mt-32">
-        <div className="relative flex justify-center align-middle">
-          <figure className="absolute mt-20 flex justify-center rounded-full border-8 border-solid border-cyan-50 shadow-lg sm:top-0 md:-top-40 lg:-top-40">
-            <img
-              src={foto}
-              alt=""
-              className="flex h-40 w-full rounded-full border-4 border-solid border-cyan-700 lg:h-56"
-            />
+      <section className="flex justify-center">
+        <div className="md:mt-20">
+          <figure className="lg:w-50 mx-auto mb-5 mt-32 w-60 justify-center rounded-full border-4 border-solid border-neutral-50 shadow-lg dark:border-sky-400 dark:shadow-none md:mt-10 md:w-60 lg:border-8 2xl:w-96">
+            {!darkMode ? (
+              <img
+                src={fotoWhite}
+                alt=""
+                className="w-full rounded-full border-4 border-solid border-sky-400 dark:border-neutral-900 lg:border-4"
+              />
+            ) : (
+              <img
+                src={foto}
+                alt=""
+                className="w-full rounded-full border-4 border-solid border-sky-400 dark:border-neutral-900 lg:border-4"
+              />
+            )}
           </figure>
-        </div>
-        <div className="mt-40 flex h-64 w-64 flex-col place-content-end rounded-2xl bg-blue-200 shadow-lg sm:mt-40 sm:w-80 md:mt-0 md:w-80 lg:mt-10 lg:h-72 lg:w-96">
-          <h1 className="text-16px text-center font-victorMonoBold text-cyan-900 lg:text-2xl">
-            Leonardo Gabriel Puebla
-          </h1>
-          <h2 className="text-center font-dancingScript text-3xl text-cyan-900">FrontEnd Developer</h2>
+          <h2 className="px-2 text-center font-victorMonoBold text-3xl text-neutral-950 dark:text-white md:text-3xl lg:text-4xl lg:tracking-wider xl:mb-5 xl:text-4xl xl:tracking-widest">
+            Hello, I'm
+          </h2>
+          <p className="mb-4 mt-2 text-center font-victorMonoBold text-4xl tracking-wider text-sky-400 xl:text-5xl 2xl:text-6xl">
+            Leonardo
+          </p>
+          <h2 className="mt-3 px-1 text-center font-Raleway text-2xl tracking-wider text-neutral-950 dark:text-white sm:text-3xl md:text-3xl lg:text-4xl xl:mt-5 2xl:mt-10 2xl:text-5xl">
+            FrontEnd Developer
+          </h2>
           <div className="my-5 flex justify-center gap-6">
-            <div className="flex h-max animate-pulse items-center rounded-lg border-2 border-solid border-cyan-800">
+            <div className="flex h-max animate-pulse items-center rounded-lg border-2 border-solid border-sky-400">
               <a href="https://twitter.com/LeonardoPu65507">
-                <Twitter className="w-10 cursor-pointer fill-cyan-700" alt="red social Twitter" />
+                <Twitter
+                  className="w-10 cursor-pointer fill-sky-400 dark:fill-white md:p-1 2xl:w-16 2xl:p-2"
+                  alt="red social Twitter"
+                />
               </a>
             </div>
-            <div className="flex animate-pulse items-center rounded-lg border-2 border-solid border-cyan-800">
+            <div className="flex animate-pulse items-center rounded-lg border-2 border-solid border-sky-400">
               <a href="https://github.com/Leonardo-Robot-PostRock?tab=repositories">
-                <Github className="w-10 cursor-pointer fill-cyan-700" alt="repositorio Github" />
+                <Github
+                  className="w-10 cursor-pointer fill-sky-400 dark:fill-white 2xl:w-16"
+                  alt="repositorio Github"
+                />
               </a>
             </div>
-            <div className="flex animate-pulse items-center rounded-lg border-2 border-solid border-cyan-800">
+            <div className="flex animate-pulse items-center rounded-lg border-2 border-solid border-sky-400">
               <a href="https://www.linkedin.com/in/leonardo-puebla-1a2b71229/">
-                <Linkedin className="w-10 cursor-pointer fill-cyan-700" alt="red social Linkedin" />
+                <Linkedin
+                  className="w-10 cursor-pointer fill-sky-400 dark:fill-white 2xl:w-16"
+                  alt="red social Linkedin"
+                />
               </a>
             </div>
           </div>
         </div>
-      </section>
-
-      <section className="flex h-auto sm:w-max md:w-2/4">
-        <PresentationWithEffects />
       </section>
     </main>
   );
