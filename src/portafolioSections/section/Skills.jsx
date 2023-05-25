1b06a39cbea487017cf6ffa91b25b4312578cc34
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-25 20:57:12 -0300

    se crea componente skills

---
+++ src/portafolioSections/section/Skills.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Skills = () => {
+  return (
+    <div className="h-screen dark:bg-neutral-900 dark:text-slate-200" id="skills">
+      Skills
+    </div>
+  );
+};
