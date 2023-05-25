67cf29beb094979ea63449484d091fe6aabf91f0
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-25 20:23:25 -0300

    se cambian colores

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -8,9 +8,9 @@ export const Navbar = ({ toggleDarkMode, darkMode, savedDarkMode }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="fixed z-10 flex w-screen min-w-280 flex-wrap items-center justify-between bg-white p-4 font-baskerville text-cyan-600 shadow-md dark:bg-gray-950 dark:text-gray-100">
-      <div className="flex flex-shrink-0 flex-wrap items-center justify-center lg:mr-72">
-        <h2 className="w-100 mx-4 h-10 font-dancingScript text-4xl md:mx-6 md:text-5xl">Leolux</h2>
+    <nav className="fixed z-10 flex w-screen min-w-280 flex-wrap items-center justify-between bg-white p-4 font-Raleway text-neutral-950 shadow-md dark:bg-neutral-950 dark:text-gray-100">
+      <div className="flex flex-shrink-0 flex-wrap items-center justify-center align-middle lg:mr-72">
+        <h2 className="w-100 mx-4 text-2xl md:mx-6 md:text-4xl">Leolux</h2>
         <ToggleDarkMode toggleDarkMode={toggleDarkMode} darkMode={darkMode} savedDarkMode={savedDarkMode} />
       </div>
       {/* Menu Hamburger */}
@@ -22,8 +22,8 @@ export const Navbar = ({ toggleDarkMode, darkMode, savedDarkMode }) => {
         <div className="sm:text-sm md:text-lg lg:mr-20 lg:flex lg:flex-grow lg:justify-end">
           <HashLink
             onClick={() => setisActiveLink('home')}
-            className={`mr-8 block max-w-max py-3 text-cyan-600 transition duration-300 hover:text-gray-400 dark:text-gray-200 dark:hover:text-green-400 lg:mt-0 lg:inline-block ${
-              isActiveLink === 'home' ? 'border-b-2 border-cyan-800 dark:border-green-400' : ''
+            className={`max-w-max dark:hover:text-skborder-sky-400 mr-8 block py-3 text-neutral-950 transition duration-300 hover:text-sky-500 dark:text-white dark:hover:text-sky-400 lg:mt-0 lg:inline-block ${
+              isActiveLink === 'home' ? 'border-b-2 border-sky-400 dark:border-sky-400' : ''
             }`}
             smooth
             to={'/#home'}>
@@ -31,8 +31,8 @@ export const Navbar = ({ toggleDarkMode, darkMode, savedDarkMode }) => {
           </HashLink>
           <HashLink
             onClick={() => setisActiveLink('about')}
-            className={`mr-8 block max-w-max py-3 text-cyan-600 transition duration-300 hover:text-gray-400 dark:text-gray-100 dark:hover:text-green-400 lg:mt-0  lg:inline-block ${
-              isActiveLink === 'about' ? 'border-b-2 border-cyan-800 dark:border-green-400' : ''
+            className={`max-w-max dark:hover:text-skborder-sky-400 mr-8 block py-3 text-neutral-950 transition duration-300 hover:text-sky-500 dark:text-white dark:hover:text-sky-400 lg:mt-0  lg:inline-block ${
+              isActiveLink === 'about' ? 'border-b-2 border-sky-400 dark:border-sky-400' : ''
             }`}
             smooth
             to={'/#about'}>
@@ -40,8 +40,8 @@ export const Navbar = ({ toggleDarkMode, darkMode, savedDarkMode }) => {
           </HashLink>
           <HashLink
             onClick={() => setisActiveLink('porfolio')}
-            className={`mr-8 block max-w-max py-3 text-cyan-600 transition duration-300 hover:text-gray-400 dark:text-gray-200 dark:hover:text-green-400 lg:mt-0 lg:inline-block ${
-              isActiveLink === 'porfolio' ? 'border-b-2 border-cyan-800 dark:border-green-400' : ''
+            className={`max-w-max dark:hover:text-skborder-sky-400 mr-8 block py-3 text-neutral-950 transition duration-300 hover:text-sky-500 dark:text-white dark:hover:text-sky-400 lg:mt-0 lg:inline-block ${
+              isActiveLink === 'porfolio' ? 'border-b-2 border-sky-400 dark:border-sky-400' : ''
             }`}
             smooth
             to={'/#portfolio'}>
@@ -49,8 +49,8 @@ export const Navbar = ({ toggleDarkMode, darkMode, savedDarkMode }) => {
           </HashLink>
           <HashLink
             onClick={() => setisActiveLink('contact')}
-            className={`mr-8 block max-w-max py-3 text-cyan-600 transition duration-300 hover:text-gray-400 dark:text-gray-200 dark:hover:text-green-400 lg:mt-0 lg:inline-block ${
-              isActiveLink === 'contact' ? 'border-b-2 border-cyan-800 dark:border-green-400' : ''
+            className={`max-w-max dark:hover:text-skborder-sky-400 mr-8 block py-3 text-neutral-950 transition duration-300 hover:text-sky-500 dark:text-white dark:hover:text-sky-400 lg:mt-0 lg:inline-block ${
+              isActiveLink === 'contact' ? 'border-b-2 border-sky-400 dark:border-sky-400' : ''
             }`}
             smooth
             to={'/#contact'}>
