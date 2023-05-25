3bd7323a86980f377e4bf662f05622a4f8cb08cf
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-25 20:56:05 -0300

    se agrega skills

--- src/portafolioSections/pages/Main.jsx
+++ src/portafolioSections/pages/Main.jsx
@@ -3,12 +3,14 @@ import { Home } from '../section/Home';
 import { About } from '../section/About';
 import { Portfolio } from '../section/Portfolio';
 import { Contact } from './../section/Contact';
+import { Skills } from '../section/Skills';
 
 export const Main = ({ darkMode }) => {
   return (
     <main className="bg-slate-100 dark:bg-neutral-900">
       <Home darkMode={darkMode} />
       <About />
+      <Skills />
       <Portfolio />
       <Contact />
     </main>
