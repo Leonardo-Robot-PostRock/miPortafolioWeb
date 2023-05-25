ee1fc4bd0ce7b4b1407fdaa43713f340323ed49c
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-25 20:21:57 -0300

    recibe props

--- src/portafolioSections/pages/Main.jsx
+++ src/portafolioSections/pages/Main.jsx
@@ -4,13 +4,13 @@ import { About } from '../section/About';
 import { Portfolio } from '../section/Portfolio';
 import { Contact } from './../section/Contact';
 
-export const Main = () => {
+export const Main = ({ darkMode }) => {
   return (
-    <>
-      <Home />
+    <main className="bg-slate-100 dark:bg-neutral-900">
+      <Home darkMode={darkMode} />
       <About />
       <Portfolio />
       <Contact />
-    </>
+    </main>
   );
 };
