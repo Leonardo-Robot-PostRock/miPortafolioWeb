fdc1b295eaf9f0f10e5175ab5e221e22e51cbb51
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-26 05:55:14 -0300

    ajustes de tamaño de letra y margenes

--- src/portafolioSections/section/Home.jsx
+++ src/portafolioSections/section/Home.jsx
@@ -8,11 +8,11 @@ import { ReactComponent as Linkedin } from '../../assets/icons/linkedin.svg';
 export const Home = ({ darkMode }) => {
   return (
     <main
-      className="flex h-screen min-w-280 flex-col place-content-center items-center bg-slate-100 font-victorMono dark:bg-neutral-900 dark:text-slate-300 md:mx-28"
+      className="flex h-screen min-w-280 place-content-center items-center bg-slate-100 font-victorMono dark:bg-neutral-900 dark:text-slate-300 md:mx-28 md:h-max"
       id="home">
-      <section className="flex justify-center">
-        <div className="md:mt-20">
-          <figure className="lg:w-50 mx-auto mb-5 mt-32 w-60 justify-center rounded-full border-4 border-solid border-neutral-50 shadow-lg dark:border-sky-400 dark:shadow-none md:mt-10 md:w-60 lg:border-8 2xl:w-96">
+      <section className="mt-32 flex justify-center md:mt-0 2xl:mt-10">
+        <div className="md:mt-16">
+          <figure className="lg:w-50 mx-auto mb-5 w-44 justify-center rounded-full border-4 border-solid border-neutral-300 shadow-lg dark:border-sky-400 dark:shadow-none md:mt-10 md:w-44 lg:border-8 2xl:w-72">
             {!darkMode ? (
               <img
                 src={fotoWhite}
@@ -27,17 +27,17 @@ export const Home = ({ darkMode }) => {
               />
             )}
           </figure>
-          <h2 className="px-2 text-center font-victorMonoBold text-3xl text-neutral-950 dark:text-white md:text-3xl lg:text-4xl lg:tracking-wider xl:mb-5 xl:text-4xl xl:tracking-widest">
+          <h2 className="px-2 text-center font-Kanit text-3xl text-neutral-950 dark:text-white md:text-3xl lg:text-3xl lg:tracking-wider xl:text-4xl xl:tracking-widest">
             Hello, I'm
           </h2>
-          <p className="mb-4 mt-2 text-center font-victorMonoBold text-4xl tracking-wider text-sky-400 xl:text-5xl 2xl:text-6xl">
+          <p className="mb-4 text-center font-Raleway text-4xl tracking-wider text-sky-400 lg:text-4xl xl:text-5xl 2xl:text-6xl">
             Leonardo
           </p>
-          <h2 className="mt-3 px-1 text-center font-Raleway text-2xl tracking-wider text-neutral-950 dark:text-white sm:text-3xl md:text-3xl lg:text-4xl xl:mt-5 2xl:mt-10 2xl:text-5xl">
+          <h2 className="xl:text-1xl mt-3 px-1 text-center font-victorMonoBold text-2xl tracking-wider text-neutral-950 dark:text-white sm:text-3xl md:text-3xl lg:text-2xl xl:mt-5 2xl:mt-10 2xl:text-5xl">
             FrontEnd Developer
           </h2>
           <div className="my-5 flex justify-center gap-6">
-            <div className="flex h-max animate-pulse items-center rounded-lg border-2 border-solid border-sky-400">
+            <div className="flex h-max animate-pulse items-center rounded-lg border-2 border-solid border-sky-400 dark:border-white">
               <a href="https://twitter.com/LeonardoPu65507">
                 <Twitter
                   className="w-10 cursor-pointer fill-sky-400 p-1 dark:fill-white 2xl:w-16 2xl:p-2"
@@ -45,7 +45,7 @@ export const Home = ({ darkMode }) => {
                 />
               </a>
             </div>
-            <div className="flex animate-pulse items-center rounded-lg border-2 border-solid border-sky-400">
+            <div className="flex animate-pulse items-center rounded-lg border-2 border-solid border-sky-400 dark:border-white">
               <a href="https://github.com/Leonardo-Robot-PostRock?tab=repositories">
                 <Github
                   className="w-10 cursor-pointer fill-sky-400 dark:fill-white 2xl:w-16"
@@ -53,7 +53,7 @@ export const Home = ({ darkMode }) => {
                 />
               </a>
             </div>
-            <div className="flex animate-pulse items-center rounded-lg border-2 border-solid border-sky-400">
+            <div className="flex animate-pulse items-center rounded-lg border-2 border-solid border-sky-400 dark:border-white">
               <a href="https://www.linkedin.com/in/leonardo-puebla-1a2b71229/">
                 <Linkedin
                   className="w-10 cursor-pointer fill-sky-400 dark:fill-white 2xl:w-16"
