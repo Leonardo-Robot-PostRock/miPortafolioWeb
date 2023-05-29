df899a066ba06e2ecc6f5c16dd27cced92a1c1f3
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-29 04:27:04 -0300

    se agranda margen

--- src/portafolioSections/section/Skills.jsx
+++ src/portafolioSections/section/Skills.jsx
@@ -6,7 +6,7 @@ export const Skills = (darkMode) => {
   return (
     <main className="relavite flex h-screen min-w-280 justify-center md:mx-0" id="skills">
       <section className="absolute mx-auto min-w-280">
-        <h1 className="mb-40 mt-32 text-center font-Raleway text-4xl dark:text-white md:mb-20 md:text-5xl 2xl:mb-32">
+        <h1 className="mb-52 mt-32 text-center font-Raleway text-4xl dark:text-white md:mb-20 md:text-5xl 2xl:mb-36">
           Skills
         </h1>
         <div className="mx-2 rounded-lg p-4 md:mx-52 2xl:mx-auto 2xl:w-7/12 2xl:p-16">
