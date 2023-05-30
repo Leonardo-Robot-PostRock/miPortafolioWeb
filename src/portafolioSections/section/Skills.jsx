1c755ed4166d066618626c5e5200346c47369a0c
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-30 03:09:21 -0300

    manejo de z-index dinamico y display

--- src/portafolioSections/section/Skills.jsx
+++ src/portafolioSections/section/Skills.jsx
@@ -1,15 +1,31 @@
 import { ParticlesComponent } from '../components/Particle';
 
-import React from 'react';
+import React, { useState } from 'react';
 
-export const Skills = (darkMode) => {
+export const Skills = ({ darkMode }) => {
+  const [zIndex, setZIndex] = useState(true);
+  const [count, setCount] = useState(0);
+
+  const handleZIndex = () => {
+    setZIndex((prevState) => !prevState);
+    setCount((prevCount) => prevCount + 1);
+    if (count === 2) {
+      setCount(0);
+    }
+  };
+
   return (
-    <main className="relavite flex h-screen min-w-280 items-center justify-center md:mx-0" id="skills">
-      <section className="absolute mx-auto min-w-280">
-        <h1 className="mb-5 text-center font-Raleway text-4xl dark:text-white sm:mb-32 md:mb-20 md:text-5xl 2xl:mb-28 2xl:text-6xl">
-          Skills
-        </h1>
-        <div className="mx-2 rounded-lg p-4 md:mx-52 2xl:mx-auto 2xl:w-7/12 2xl:p-16">
+    <main className="relavite flex h-screen min-w-280 flex-col items-center justify-center md:mx-0" id="skills">
+      <h1
+        className={`z-30 mb-96 flex cursor-pointer items-center  rounded-lg p-3 text-center font-Raleway text-4xl hover:bg-sky-400 hover:text-white dark:text-white hover:dark:rounded-lg hover:dark:bg-sky-400 hover:dark:text-white sm:mb-64 md:mb-72 md:text-5xl 2xl:mb-80 2xl:text-6xl ${
+          count === 2 ? 'opacity-5' : ''
+        }`}
+        onClick={handleZIndex}>
+        <span className="material-symbols-outlined pr-2">left_click</span>
+        Skills
+      </h1>
+      <section className={`absolute mx-auto min-w-280 ${zIndex === true ? 'z-10' : 'z-0'}`}>
+        <div className={`mx-2 rounded-lg p-4 md:mx-52 2xl:mx-auto 2xl:w-7/12 2xl:p-16 ${count === 2 ? 'hidden' : ''}`}>
           <h3 className="mb-1 font-Kanit text-2xl dark:text-white 2xl:mb-4 2xl:text-4xl">Tengo conocimientos en:</h3>
           <p className=" font-victorMono text-sm dark:text-white 2xl:text-lg">
             Node.js, JS, Angular, React, Angular Material, PrimeNG, PrimeFlex, MaterialUi,Tailwind CSS, CSS, HTML,
@@ -18,7 +34,7 @@ export const Skills = (darkMode) => {
           </p>
         </div>
       </section>
-      <ParticlesComponent className="absolute" darkMode={darkMode} id="skills" />
+      <ParticlesComponent darkMode={darkMode} id="tsparticles" zIndex={zIndex} count={count} />
     </main>
   );
 };
