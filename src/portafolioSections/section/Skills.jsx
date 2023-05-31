4ae783ae73dc68ae44a13e9da386de95178a992f
Leonardo Puebla <leonardo38361@gmail.com>
2023-05-31 00:16:48 -0300

    se mejora logica de renderizado

--- src/portafolioSections/section/Skills.jsx
+++ src/portafolioSections/section/Skills.jsx
@@ -7,9 +7,10 @@ export const Skills = ({ darkMode }) => {
   const [count, setCount] = useState(0);
 
   const handleZIndex = () => {
-    setZIndex((prevState) => !prevState);
+    console.log(zIndex, count);
     setCount((prevCount) => prevCount + 1);
-    if (count === 2) {
+    if (count === 3) {
+      setZIndex((prevState) => !prevState);
       setCount(0);
     }
   };
@@ -20,16 +21,31 @@ export const Skills = ({ darkMode }) => {
         className={`z-30 mb-96 flex cursor-pointer items-center  rounded-lg p-3 text-center font-Raleway text-4xl 
         transition-colors duration-500 hover:bg-sky-400 hover:text-white hover:transition-all hover:duration-500 dark:text-white hover:dark:rounded-lg hover:dark:bg-sky-400 
         hover:dark:text-white sm:mb-64 md:mb-72 md:text-5xl 2xl:mb-80 2xl:text-6xl ${
-          count === 2 ? 'animate-pulse opacity-5 hover:bg-inherit hover:text-current dark:hover:bg-inherit' : ''
+          (count === 0 && 'animate-bounce opacity-100 hover:bg-inherit hover:text-current dark:hover:bg-none') ||
+          (count === 3 && 'animate-pulse opacity-5 hover:bg-inherit hover:text-current dark:hover:bg-none')
         }`}
         onClick={handleZIndex}>
         <span className="material-symbols-outlined pr-2">left_click</span>
         Skills
       </h1>
-      <section className={`absolute mx-auto min-w-280 ${zIndex === true ? 'z-20' : count === 1 && 'z-20'}`}>
-        <div className={`mx-2 rounded-lg p-4 md:mx-52 2xl:mx-auto 2xl:w-7/12 2xl:p-16 ${count === 2 && 'hidden'}`}>
+      <section
+        className={`absolute mx-auto min-w-280 md:mt-28 ${
+          count === 0
+            ? 'hidden'
+            : count === 1
+            ? 'animate__animated animate__bounceInDown z-20'
+            : count === 2
+            ? 'text-neutral-800 dark:text-white'
+            : count === 3 && 'hidden'
+        }`}>
+        <div
+          className={`mx-2 rounded-lg p-4 md:mx-52 2xl:mx-auto 2xl:w-7/12 2xl:p-16 ${count === 3 ? 'hidden' : 'z-0'}`}>
           <h3 className="mb-1 font-Kanit text-2xl dark:text-white 2xl:mb-4 2xl:text-4xl">Tengo conocimientos en:</h3>
-          <p className=" font-victorMono text-sm dark:text-white 2xl:text-lg">
+          <p
+            className={`font-victorMono text-sm 2xl:text-lg ${
+              count === 1 &&
+              'z-20 before:absolute before:inset-0 before:block before:-skew-y-3 before:bg-sky-400 before:bg-opacity-10 dark:text-white md:before:-skew-y-1 2xl:before:-skew-y-3'
+            }`}>
             Node.js, JS, Angular, React, Angular Material, PrimeNG, PrimeFlex, MaterialUi,Tailwind CSS, CSS, HTML,
             Webpack, Vite, sql, mysql y mongodb. Experiencia en desarrollo web frontend y backend, creación de
             aplicaciones dinámicas, gestión del estado y diseño de interfaces atractivas y funcionales.
