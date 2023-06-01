2c7e2f87356b527b50c73694755d6d7d03f41165
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-01 17:07:52 -0300

    estructura de portafolio primer version

--- src/portafolioSections/section/Portfolio.jsx
+++ src/portafolioSections/section/Portfolio.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import iphoneX from '../../assets/images/deviceShots/iphoneX.png';
+import ipadMini from '../../assets/images/deviceShots/ipadMini.png';
+import macBookPro16 from '../../assets/images/deviceShots/macBookPro16.png';
+
 export const Portfolio = () => {
   return (
-    <div className="h-screen" id="portfolio">
-      Portfolio
-    </div>
+    <main className="h-screen min-w-280" id="portfolio">
+      <section className="mt-20 flex flex-col justify-center">
+        <h1 className="mb-5 text-center font-Raleway text-3xl dark:text-white">Portfolio</h1>
+        <div className="flex h-full">
+          <figure className="relative w-full">
+            <figure className="z-10 flex w-full justify-evenly">
+              <img className="top-0 z-10 h-44" src={iphoneX} alt="" />
+              <img className="top-10 z-10 h-56" src={ipadMini} alt="" />
+            </figure>
+            <img className="top-0 z-0 w-full" src={macBookPro16} alt="" />
+          </figure>
+        </div>
+      </section>
+    </main>
   );
 };
