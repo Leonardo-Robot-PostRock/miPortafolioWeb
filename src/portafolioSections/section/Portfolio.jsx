90ff5f0b388f9266adfaf0239ac5b07f1283c3d5
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-01 17:11:02 -0300

    diseño para celulares

--- src/portafolioSections/section/Portfolio.jsx
+++ src/portafolioSections/section/Portfolio.jsx
@@ -6,17 +6,17 @@ import macBookPro16 from '../../assets/images/deviceShots/macBookPro16.png';
 
 export const Portfolio = () => {
   return (
-    <main className="h-screen min-w-280" id="portfolio">
+    <main className="h-max min-w-280" id="portfolio">
       <section className="mt-20 flex flex-col justify-center">
         <h1 className="mb-5 text-center font-Raleway text-3xl dark:text-white">Portfolio</h1>
         <div className="flex h-full">
-          <figure className="relative w-full">
-            <figure className="z-10 flex w-full justify-evenly">
+          <div className="">
+            <figure className="z-10 flex w-full items-center justify-evenly">
               <img className="top-0 z-10 h-44" src={iphoneX} alt="" />
               <img className="top-10 z-10 h-56" src={ipadMini} alt="" />
             </figure>
-            <img className="top-0 z-0 w-full" src={macBookPro16} alt="" />
-          </figure>
+            <img className="top-0 z-0 w-96" src={macBookPro16} alt="" />
+          </div>
         </div>
       </section>
     </main>
