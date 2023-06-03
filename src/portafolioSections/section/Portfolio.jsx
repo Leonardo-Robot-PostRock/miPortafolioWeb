cf309a74bfad46c82677a3a6ed418e30086b26b6
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-03 01:12:34 -0300

    se quita cuadro azul al hacer click o imagen tipo boton

--- src/portafolioSections/section/Portfolio.jsx
+++ src/portafolioSections/section/Portfolio.jsx
@@ -6,17 +6,22 @@ import macBookPro16 from '../../assets/images/deviceShots/macBookPro16.png';
 
 export const Portfolio = () => {
   const [animation, setAnimation] = useState('');
+  const [animationTwo, setAnimationTwo] = useState('');
   const [contador, setContador] = useState(0);
 
   useEffect(() => {
-    setTimeout(() => {
+    const timeout = setTimeout(() => {
       setContador((prevValue) => prevValue + 1);
+
       if (contador === 3) {
-        setAnimation('animate__animated animate__fadeInDown');
+        setAnimation('animate__fadeInDown');
+        setAnimationTwo('animate__fadeInLeft');
         setContador(0);
       }
       setTimeout(() => {
         setAnimation('');
+        setAnimationTwo('');
+        return clearTimeout(timeout);
       }, 2000);
     }, [1000]);
   }, [contador]);
@@ -27,10 +32,10 @@ export const Portfolio = () => {
         <h1 className="mb-5 text-center font-Raleway text-3xl dark:text-white">Portfolio</h1>
         <div className="flex h-full justify-center">
           <div className="relative">
-            <img className="top-0 z-0 w-96" src={macBookPro16} alt="Mac Book Pro 16" />
-            <figure className={`absolute top-44 z-10 flex w-full items-center justify-evenly ${animation}`}>
-              <img className="h-44" src={iphoneX} alt="Iphone X" />
-              <img className="h-72" src={ipadMini} alt="Ipad Mini" />
+            <img className={`animate__animated z-0 w-96 ${animationTwo}`} src={macBookPro16} alt="Mac Book Pro 16" />
+            <figure className={`animate__animated abosolute z-10 flex w-full items-center justify-evenly ${animation}`}>
+              <img className="relative z-20 h-44  md:-right-44 md:bottom-64" src={iphoneX} alt="Iphone X" />
+              <img className="relative h-72 md:-right-20 md:bottom-72" src={ipadMini} alt="Ipad Mini" />
             </figure>
           </div>
         </div>
