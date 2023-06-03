1840bbc2c7a4ab7f7f2df287db4e6020e580f7d5
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-03 01:28:06 -0300

    media queries para celulares

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      //Galaxy Fold
+      'xs': '280px',
+      //Se toma como referencia iphoneX
+      '375': '375px',
+      ...defaultTheme.screens
+    },
     fontFamily: {
       'Raleway': ['Raleway', 'sans-serif'],
       'Kanit': ['Kanit', 'sans-serif'],
