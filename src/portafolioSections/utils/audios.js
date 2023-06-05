1dbcf62f0c55644a75d98b061c541c0b9406e172
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-05 01:33:10 -0300

    se crea objeto de audios

---
+++ src/portafolioSections/utils/audios.js
@@ -0,0 +1,20 @@
+import BulbularSwirl from '../../assets/music/BulbularSwirl.mp3';
+import PhotonRez from '../../assets/music/PhotonRez.mp3';
+import QuantumSingularity from '../../assets/music/QuantumSingularity.mp3';
+
+const playlist = [
+    {
+        "title": 'NFS High Stakes: Bulbular Swirl',
+        "url": BulbularSwirl
+    },
+    {
+        "title": 'NFS High Stakes: Photon Rez',
+        "url": PhotonRez
+    },
+    {
+        "title": 'NFS High Stakes: Quantum Singularity',
+        "url": QuantumSingularity
+    },
+]
+
+export default playlist;
