aca5e4a47cecb9e6701d45022b081c23383afe50
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-05 06:11:23 -0300

    nuevo audio

--- src/portafolioSections/utils/audios.js
+++ src/portafolioSections/utils/audios.js
@@ -1,6 +1,7 @@
 import BulbularSwirl from '../../assets/music/BulbularSwirl.mp3';
 import PhotonRez from '../../assets/music/PhotonRez.mp3';
 import QuantumSingularity from '../../assets/music/QuantumSingularity.mp3';
+import MacQuayle from '../../assets/music/whatsyourask.mp3';
 
 const playlist = [
     {
@@ -14,6 +15,10 @@ const playlist = [
     {
         "title": 'Quantum Singularity',
         "url": QuantumSingularity
+    },
+    {
+        "title": '1.0_8-whatsyourask',
+        "url": MacQuayle
     },
 ]
 
