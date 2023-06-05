90a0885ff844d31f2f9c2eec4ab3b898a50478c6
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-05 01:39:48 -0300

    se optimiza el código de Navbar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -7,6 +7,18 @@ export const Navbar = ({ toggleDarkMode, darkMode, savedDarkMode }) => {
   const [isActiveLink, setisActiveLink] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
+  const navLinks = [
+    { id: 'home', text: 'Home' },
+    { id: 'about', text: 'About' },
+    { id: 'skills', text: 'Skills' },
+    { id: 'portfolio', text: 'Portfolio' },
+    { id: 'contact', text: 'Contact' }
+  ];
+
+  const handleLinkClick = (id) => {
+    setisActiveLink(id);
+  };
+
   return (
     <nav className="fixed z-40 flex w-screen min-w-280 flex-wrap items-center justify-between bg-white p-4 font-Raleway text-neutral-950 shadow-md dark:bg-neutral-950 dark:text-gray-100">
       <div className="flex flex-shrink-0 flex-wrap items-center justify-center align-middle">
@@ -20,51 +32,18 @@ export const Navbar = ({ toggleDarkMode, darkMode, savedDarkMode }) => {
           isOpen ? 'animate__animated animate__fadeInRight block' : 'hidden'
         }`}>
         <div className="sm:text-sm md:text-lg lg:mr-20 lg:flex lg:flex-grow lg:justify-end">
-          <HashLink
-            onClick={() => setisActiveLink('home')}
-            className={`dark:hover:text-skborder-sky-400 mr-8 block max-w-max py-3 text-neutral-950 transition duration-300 hover:text-sky-500 dark:text-white dark:hover:text-sky-400 lg:mt-0 lg:inline-block ${
-              isActiveLink === 'home' ? 'border-b-2 border-sky-400 dark:border-sky-400' : ''
-            }`}
-            smooth
-            to={'/#home'}>
-            Home
-          </HashLink>
-          <HashLink
-            onClick={() => setisActiveLink('about')}
-            className={`dark:hover:text-skborder-sky-400 mr-8 block max-w-max py-3 text-neutral-950 transition duration-300 hover:text-sky-500 dark:text-white dark:hover:text-sky-400 lg:mt-0  lg:inline-block ${
-              isActiveLink === 'about' ? 'border-b-2 border-sky-400 dark:border-sky-400' : ''
-            }`}
-            smooth
-            to={'/#about'}>
-            About
-          </HashLink>
-          <HashLink
-            onClick={() => setisActiveLink('skills')}
-            className={`dark:hover:text-skborder-sky-400 mr-8 block max-w-max py-3 text-neutral-950 transition duration-300 hover:text-sky-500 dark:text-white dark:hover:text-sky-400 lg:mt-0  lg:inline-block ${
-              isActiveLink === 'skills' ? 'border-b-2 border-sky-400 dark:border-sky-400' : ''
-            }`}
-            smooth
-            to={'/#skills'}>
-            Skills
-          </HashLink>
-          <HashLink
-            onClick={() => setisActiveLink('porfolio')}
-            className={`dark:hover:text-skborder-sky-400 mr-8 block max-w-max py-3 text-neutral-950 transition duration-300 hover:text-sky-500 dark:text-white dark:hover:text-sky-400 lg:mt-0 lg:inline-block ${
-              isActiveLink === 'porfolio' ? 'border-b-2 border-sky-400 dark:border-sky-400' : ''
-            }`}
-            smooth
-            to={'/#portfolio'}>
-            Portfolio
-          </HashLink>
-          <HashLink
-            onClick={() => setisActiveLink('contact')}
-            className={`dark:hover:text-skborder-sky-400 mr-8 block max-w-max py-3 text-neutral-950 transition duration-300 hover:text-sky-500 dark:text-white dark:hover:text-sky-400 lg:mt-0 lg:inline-block ${
-              isActiveLink === 'contact' ? 'border-b-2 border-sky-400 dark:border-sky-400' : ''
-            }`}
-            smooth
-            to={'/#contact'}>
-            Contact
-          </HashLink>
+          {navLinks.map((link) => (
+            <HashLink
+              key={link.id}
+              onClick={() => handleLinkClick(link.id)}
+              className={`dark:hover:text-skborder-sky-400 mr-8 block max-w-max py-3 text-neutral-950 transition duration-300 hover:text-sky-500 dark:text-white dark:hover:text-sky-400 lg:mt-0 lg:inline-block ${
+                isActiveLink === link.id ? 'border-b-2 border-sky-400 dark:border-sky-400' : ''
+              }`}
+              smooth
+              to={`/#${link.id}`}>
+              {link.text}
+            </HashLink>
+          ))}
         </div>
       </div>
     </nav>
