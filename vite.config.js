0a511ce3c2d4fdaa33960e27605b63e6dc4b63bd
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-28 02:20:13 -0300

    se configura compresión

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
+  viteCompression({
+    algorithm: 'brotliCompress',
+    ext: '.br',
+    disable: false,
+    verbose: true
+  }),
   svgr({
     svgrOptions: {
       // svgr options
