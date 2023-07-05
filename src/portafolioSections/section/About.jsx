e12b0d73943293956b81b29e6ee085d428e34a23
Leonardo <leonardo38361@gmail.com>
2023-07-05 19:49:20 -0300

    se agrega background y se agrega display flex a la caja de texto

--- src/portafolioSections/section/About.jsx
+++ src/portafolioSections/section/About.jsx
@@ -10,29 +10,34 @@ export const About = () => {
       title: 'Eloquent JavaScript',
       author: 'Marjin Haverbeke',
       image: eloquentJsImage,
-      pag: 'estoy en el capítulo 3'
+      pag: 'estoy en el capítulo 3',
     },
     {
       title: 'Python a fondo',
       author: 'Óscar Ramirez Jiménez',
       image: pythonAfondoImage,
-      pag: 'estoy en la página 58'
-    }
+      pag: 'estoy en la página 58',
+    },
   ];
 
   const personalData = {
     name: 'Leonardo Gabriel Puebla',
     fechaDeNacimiento: '22/06/1994',
     email: 'leonardo38361@gmail.com',
-    phone: '+549-2612589352'
+    phone: '+549-2612589352',
   };
 
   const [currentSlide, setCurrentSlide] = useState(0);
   const [bookAnimation, setBookAnimation] = useState('');
 
   const handleSlideChange = (increment) => {
-    setCurrentSlide((prevSlide) => (prevSlide + increment + programmingBooks.length) % programmingBooks.length);
-    const animationClass = increment === -1 ? 'animate__fadeInLeft' : 'animate__fadeInRight';
+    setCurrentSlide(
+      (prevSlide) =>
+        (prevSlide + increment + programmingBooks.length) %
+        programmingBooks.length
+    );
+    const animationClass =
+      increment === -1 ? 'animate__fadeInLeft' : 'animate__fadeInRight';
     if (bookAnimation !== `animate__animated ${animationClass}`) {
       setBookAnimation(`animate__animated ${animationClass}`);
       setTimeout(() => {
@@ -57,26 +62,33 @@ export const About = () => {
         <h2 className="mb-1 text-center font-Raleway text-3xl md:mt-24 md:text-5xl xl:mt-24 2xl:my-5 2xl:mb-5 2xl:text-6xl">
           About me
         </h2>
-        <div className="md:grid md:grid-cols-2 md:gap-24 xl:grid xl:grid-cols-2 xl:gap-x-36">
-          <div className="mx-5 leading-6 md:w-72 2xl:w-96">
-            <h3 className="p-1 text-center font-Kanit text-2xl md:text-start 2xl:text-4xl">Estudios y experiencia:</h3>
-            <p className="mt-2 p-1 font-victorMono text-sm tracking-normal 2xl:text-lg">
-              Comencé mis estudios con Java en el curso 111 Mil Programadores entre el año 2016 y 2017. Luego, reforcé
-              conocimientos en POO. En 2019 ingresé a la UTN y estudié durante todo el año. En 2021, me uní a
-              plataformas como Platzi y Udemy, donde estudié JavaScript y React. Actualmente, estoy estudiando
-              Desarrollo de Software en ies-9008 Manuel Belgrano.
+        <div className="justify-items-center overflow-hidden rounded-md bg-neutral-200 p-3 dark:bg-neutral-950 md:grid md:grid-cols-2 md:gap-24 xl:grid xl:grid-cols-2 xl:gap-x-36">
+          <div className="leading-6 md:w-72 2xl:w-96">
+            <h3 className="p-1 text-center font-Kanit text-2xl md:text-center 2xl:text-4xl">
+              Estudios y experiencia:
+            </h3>
+            <p className="mx-2 mt-2 whitespace-break-spaces p-1 font-victorMono text-sm tracking-normal md:mx-0 2xl:text-lg">
+              Comencé mis estudios con Java en el curso 111 Mil Programadores
+              entre el año 2016 y 2017. Luego, reforcé conocimientos en POO. En
+              2019 ingresé a la UTN y estudié durante todo el año. En 2021, me
+              uní a plataformas como Platzi y Udemy, donde estudié JavaScript y
+              React. Actualmente, estoy estudiando Desarrollo de Software en
+              ies-9008 Manuel Belgrano.
             </p>
-            <p className="mt-2 p-1 font-victorMonoBold text-sm 2xl:text-lg">
+            <p className="mx-2 mt-2 whitespace-break-spaces p-1 font-victorMonoBold text-sm md:mx-0 2xl:text-lg">
               Tengo 2 años de experiencia laboral como programador freelance.
             </p>
             <div className="my-2 border-b-2 border-neutral-950 dark:border-white md:hidden"></div>
-            <div className="my-2 flex flex-col place-items-center md:mt-6 2xl:mt-5">
-              <h1 className="mb-10 text-center font-Kanit text-2xl tracking-widest 2xl:mb-16 2xl:text-4xl">
+            <div className="my-1 flex flex-col place-items-center md:mt-5">
+              <h1 className="mb-4 text-center font-Kanit text-2xl tracking-widest 2xl:mb-4 2xl:text-4xl">
                 Curriculum
               </h1>
               <button className="flex w-max justify-self-center rounded-lg border-2 border-sky-400 p-2 align-middle font-victorMonoBold text-sky-400 transition-colors duration-500 hover:bg-sky-400 hover:text-white hover:transition-colors hover:duration-500 dark:border-white dark:text-white hover:dark:border-sky-400">
                 <span className="material-symbols-outlined px-1">download</span>
-                <a className="text-md px-1 2xl:text-lg" href={leonardoCv} download>
+                <a
+                  className="text-md px-1 2xl:text-lg"
+                  href={leonardoCv}
+                  download>
                   Download Curriculum
                 </a>
               </button>
@@ -84,15 +96,18 @@ export const About = () => {
           </div>
           <div className="mb-2 flex flex-col md:w-72 xl:w-96">
             <div className="mx-5 my-2 border-b-2 border-neutral-950 dark:border-white md:hidden"></div>
-            <h3 className="p-1 text-center font-Kanit text-2xl 2xl:text-4xl">📚Books I read:</h3>
+            <h3 className="p-1 text-center font-Kanit text-2xl 2xl:text-4xl">
+              📚Books I read:
+            </h3>
             <div
-              className={`relative mx-auto mb-2 mt-2 flex w-56 min-w-280 justify-center overflow-hidden p-1 md:mx-auto md:w-full md:overflow-visible 2xl:w-full`}>
+              className={`relative mx-auto my-2 flex w-56 min-w-280 justify-center p-1 md:mx-auto md:w-full md:overflow-visible 2xl:w-full`}>
               <button
                 className="absolute left-2 top-1/2 z-10 flex -translate-y-1/2 transform rounded-full bg-neutral-950 bg-opacity-50 p-2 text-white md:hover:bg-sky-400 md:hover:transition md:hover:duration-500 dark:md:hover:bg-sky-400 dark:md:hover:bg-opacity-50 xl:left-10 2xl:left-0"
                 onClick={handlePrevSlide}>
                 <span className="material-symbols-outlined">chevron_left</span>
               </button>
-              <figure className={`z-0 m-2 rounded-xl border-2 border-neutral-950 dark:border-white ${bookAnimation}`}>
+              <figure
+                className={`z-0 m-2 rounded-xl border-2 border-neutral-950 dark:border-white ${bookAnimation}`}>
                 <img
                   src={programmingBooks[currentSlide].image}
                   alt={programmingBooks[currentSlide].title}
@@ -101,8 +116,12 @@ export const About = () => {
                 <p className="mt-2 text-center font-Raleway text-sm 2xl:text-lg">
                   {programmingBooks[currentSlide].title}
                 </p>
-                <p className="2xl:text-md text-center font-Raleway text-xs">{programmingBooks[currentSlide].author}</p>
-                <p className="2xl:text-md text-center font-Kanit text-xs">{programmingBooks[currentSlide].pag}</p>
+                <p className="2xl:text-md text-center font-Raleway text-xs">
+                  {programmingBooks[currentSlide].author}
+                </p>
+                <p className="2xl:text-md text-center font-Kanit text-xs">
+                  {programmingBooks[currentSlide].pag}
+                </p>
               </figure>
               <button
                 className="absolute right-2 top-1/2 flex -translate-y-1/2 transform rounded-full bg-neutral-950 bg-opacity-50 p-2 text-white md:hover:bg-sky-400 md:hover:transition md:hover:duration-500 dark:md:hover:bg-sky-400 dark:md:hover:bg-opacity-50 xl:right-10 2xl:right-0"
@@ -112,12 +131,22 @@ export const About = () => {
             </div>
             <div className="mx-5 my-2 border-b-2 border-neutral-950 dark:border-white md:hidden"></div>
             <div className="mx-5 flex flex-col place-items-center md:mx-2">
-              <h2 className="mb-4 text-center font-Kanit text-2xl 2xl:mt-5 2xl:text-4xl">Personal Information</h2>
+              <h2 className="mb-4 text-center font-Kanit text-2xl 2xl:mt-5 2xl:text-4xl">
+                Personal Information
+              </h2>
               <div className="flex w-64 flex-col rounded-lg border-2 border-dotted border-sky-400 p-2 dark:border-white sm:align-middle md:w-full 2xl:p-4">
-                <p className="font-victorMono text-sm 2xl:text-lg">Name: {personalData.name}</p>
-                <p className="font-victorMono text-sm 2xl:text-lg">Nacimiento: {personalData.fechaDeNacimiento}</p>
-                <p className="font-victorMono text-sm 2xl:text-lg">Email: {personalData.email}</p>
-                <p className="font-victorMono text-sm tracking-widest 2xl:text-lg">Phone: {personalData.phone}</p>
+                <p className="font-victorMono text-sm 2xl:text-lg">
+                  Name: {personalData.name}
+                </p>
+                <p className="font-victorMono text-sm 2xl:text-lg">
+                  Nacimiento: {personalData.fechaDeNacimiento}
+                </p>
+                <p className="font-victorMono text-sm 2xl:text-lg">
+                  Email: {personalData.email}
+                </p>
+                <p className="font-victorMono text-sm tracking-widest 2xl:text-lg">
+                  Phone: {personalData.phone}
+                </p>
               </div>
             </div>
           </div>
