f45e46930de2ff034a4bbe69ea1fd8f245a6841f
Leonardo <leonardo38361@gmail.com>
2023-07-06 03:44:44 -0300

    arreglo de imagenes

---
+++ src/portafolioSections/utils/proyectsWeb.js
@@ -0,0 +1,54 @@
+import busquedaPaises from '../../assets/images/proyects/busquedaPaises.png';
+import gifExpertApp from '../../assets/images/proyects/gifExpertApp.png';
+import gifWeb from '../../assets/images/proyects/gifWeb.png';
+import bataBit from '../../assets/images/proyects/bataBit.png';
+import todoMachine from '../../assets/images/proyects/todoMachine.png';
+import searchGitHubUsers from '../../assets/images/proyects/searchGitHubUsers.png';
+import yardSale from '../../assets/images/proyects/yardSale.png';
+
+const proyects = [
+  {
+    image: busquedaPaises,
+    title: 'Búsqueda de Países',
+    description: 'Aplicación realizada en Angular',
+    link: 'https://busqueda-paises-leonardo.netlify.app/',
+  },
+  {
+    image: gifExpertApp,
+    title: 'Búsqueda de Gifs',
+    description: 'Aplicación realizada en React',
+    link: 'https://gif-expert-react-leonardo.netlify.app/',
+  },
+  {
+    image: gifWeb,
+    title: 'Búsqueda de Gifs',
+    description: 'Aplicación realizada en Angular',
+    link: 'https://gifsweb.netlify.app/',
+  },
+  {
+    image: bataBit,
+    title: 'BataBit',
+    description: 'Criptomonedas Demo realizado con Vanilla Js',
+    link: 'https://leonardo-robot-postrock.github.io/BataBit/',
+  },
+  {
+    image: todoMachine,
+    title: 'Todo Machine',
+    description: 'React Application web',
+    link: 'https://leonardo-robot-postrock.github.io/TODO-MACHINE/',
+  },
+  {
+    image: searchGitHubUsers,
+    title: 'Buscador de Usuarios',
+    description: 'React Application web',
+    link: 'https://materialuileonardo.netlify.app/',
+  },
+  {
+    image: yardSale,
+    title: 'E-commerce Demo',
+    description: 'React application, e-commerce aún en producción',
+    link: 'https://leonardo-robot-postrock.github.io/react-shop/',
+  },
+];
+
+export default proyects;
