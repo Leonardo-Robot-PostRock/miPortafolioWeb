8391734c996dcc17ab97d7779985ddd7f8f506be
Leonardo <leonardo38361@gmail.com>
2023-07-06 16:21:22 -0300

    mejorado del responsive

--- src/portafolioSections/section/About.jsx
+++ src/portafolioSections/section/About.jsx
@@ -62,13 +62,13 @@ export const About = () => {
         <h2 className="mb-1 text-center font-Raleway text-3xl md:mt-24 md:text-5xl xl:mt-24 2xl:my-5 2xl:mb-5 2xl:text-6xl">
           About me
         </h2>
-        <div className="justify-items-center overflow-hidden md:rounded-md bg-neutral-200 p-3 dark:bg-neutral-950 md:grid md:grid-cols-2 md:gap-24 xl:grid xl:grid-cols-2 xl:gap-x-36">
-          <div className="leading-6 md:w-72 2xl:w-96">
+        <div className="grid place-content-center overflow-hidden bg-neutral-200 p-4 dark:bg-neutral-950 md:grid md:grid-cols-2 md:gap-24 md:rounded-md xl:gap-x-36">
+          <div className="col-span-1 w-72 leading-6 md:w-72 xl:w-96">
             <h3 className="p-1 text-center font-Kanit text-2xl md:text-center 2xl:text-4xl">
               Estudios y experiencia:
             </h3>
             <div className="my-2 flex flex-col p-1 md:w-full">
-              <p className="mx-2 mt-2 whitespace-break-spaces p-1 font-victorMono text-sm tracking-normal md:mx-0 2xl:text-lg">
+              <p className="mx-2 mt-2 whitespace-break-spaces p-1 font-victorMono text-sm md:tracking-wide md:mx-0 md:text-base 2xl:text-lg">
                 Comencé mis estudios con Java en el curso 111 Mil Programadores
                 entre el año 2016 y 2017. Luego, reforcé conocimientos en POO.
                 En 2019 ingresé a la UTN y estudié durante todo el año. En 2021,
@@ -76,19 +76,21 @@ export const About = () => {
                 JavaScript y React. Actualmente, estoy estudiando Desarrollo de
                 Software en ies-9008 Manuel Belgrano.
               </p>
-              <p className="mx-2 mt-1 whitespace-break-spaces p-1 font-victorMonoBold text-sm md:mx-0 2xl:text-lg">
+              <p className="mx-2 mt-1 whitespace-break-spaces p-1 font-victorMonoBold text-sm md:mx-0 md:text-base 2xl:text-lg">
                 Tengo 2 años de experiencia laboral como programador freelance.
               </p>
             </div>
             <div className="my-2 border-b-2 border-neutral-950 dark:border-white md:hidden"></div>
             <div className="flex flex-col place-items-center">
-              <h1 className="mb-4 text-center font-Kanit text-2xl tracking-widest 2xl:mb-4 2xl:text-4xl">
+              <h1 className="mb-3 text-center font-Kanit text-2xl tracking-widest 2xl:mb-4 2xl:text-4xl">
                 Curriculum
               </h1>
-              <button className="flex w-max items-center justify-self-center rounded-lg border-2 border-sky-400 p-2 align-middle font-victorMonoBold text-sky-400 transition-colors duration-500 hover:bg-sky-400 hover:text-white hover:transition-colors hover:duration-500 dark:border-white dark:text-white hover:dark:border-sky-400">
-                <span className="material-symbols-outlined px-1">download</span>
+              <button className="flex w-max items-center rounded-lg border-2 border-sky-400 p-2 font-victorMonoBold text-sky-400 transition-colors duration-500 hover:bg-sky-400 hover:text-white hover:transition-colors hover:duration-500 dark:border-white dark:text-white hover:dark:border-sky-400">
+                <span className="material-symbols-outlined px-1 text-base font-bold">
+                  download
+                </span>
                 <a
-                  className="text-md px-1 2xl:text-lg"
+                  className="px-1 text-base 2xl:text-lg"
                   href={leonardoCv}
                   download>
                   Download Curriculum
@@ -96,7 +98,7 @@ export const About = () => {
               </button>
             </div>
           </div>
-          <div className="mb-2 flex flex-col md:w-72 xl:w-96">
+          <div className="col-span-1 w-72 md:w-72 xl:w-96">
             <div className="mx-5 my-2 border-b-2 border-neutral-950 dark:border-white md:hidden"></div>
             <h3 className="p-1 text-center font-Kanit text-2xl 2xl:text-4xl">
               📚Books I read:
@@ -104,7 +106,7 @@ export const About = () => {
             <div
               className={`relative mx-auto my-2 flex w-56 min-w-280 justify-center p-1 md:mx-auto md:w-full md:overflow-visible 2xl:w-full`}>
               <button
-                className="absolute left-2 top-1/2 z-10 flex -translate-y-1/2 transform rounded-full bg-neutral-950 bg-opacity-50 p-2 text-white md:hover:bg-sky-400 md:hover:transition md:hover:duration-500 dark:md:hover:bg-sky-400 dark:md:hover:bg-opacity-50 xl:left-10 2xl:left-0"
+                className="absolute -left-2 top-1/2 z-10 flex -translate-y-1/2 transform rounded-full bg-neutral-950 bg-opacity-50 p-2 text-white md:hover:bg-sky-400 md:hover:transition md:hover:duration-500 dark:md:hover:bg-sky-400 dark:md:hover:bg-opacity-50 xl:left-10 2xl:left-0"
                 onClick={handlePrevSlide}>
                 <span className="material-symbols-outlined">chevron_left</span>
               </button>
@@ -113,7 +115,7 @@ export const About = () => {
                 <img
                   src={programmingBooks[currentSlide].image}
                   alt={programmingBooks[currentSlide].title}
-                  className="h-52 rounded-t-lg 2xl:h-64"
+                  className="h-64 rounded-t-lg xl:h-52 2xl:h-64"
                 />
                 <p className="mt-2 text-center font-Raleway text-sm 2xl:text-lg">
                   {programmingBooks[currentSlide].title}
@@ -126,7 +128,7 @@ export const About = () => {
                 </p>
               </figure>
               <button
-                className="absolute right-2 top-1/2 flex -translate-y-1/2 transform rounded-full bg-neutral-950 bg-opacity-50 p-2 text-white md:hover:bg-sky-400 md:hover:transition md:hover:duration-500 dark:md:hover:bg-sky-400 dark:md:hover:bg-opacity-50 xl:right-10 2xl:right-0"
+                className="absolute -right-2 top-1/2 flex -translate-y-1/2 transform rounded-full bg-neutral-950 bg-opacity-50 p-2 text-white md:hover:bg-sky-400 md:hover:transition md:hover:duration-500 dark:md:hover:bg-sky-400 dark:md:hover:bg-opacity-50 xl:right-10 2xl:right-0"
                 onClick={handleNextSlide}>
                 <span className="material-symbols-outlined">chevron_right</span>
               </button>
@@ -137,16 +139,16 @@ export const About = () => {
                 Personal Information
               </h2>
               <div className="flex w-64 flex-col rounded-lg border-2 border-dotted border-sky-400 p-2 dark:border-white sm:align-middle md:w-full 2xl:p-4">
-                <p className="font-victorMono text-sm 2xl:text-lg">
+                <p className="font-victorMono text-sm md:text-base 2xl:text-lg">
                   Name: {personalData.name}
                 </p>
-                <p className="font-victorMono text-sm 2xl:text-lg">
+                <p className="font-victorMono text-sm md:text-base 2xl:text-lg">
                   Nacimiento: {personalData.fechaDeNacimiento}
                 </p>
-                <p className="font-victorMono text-sm 2xl:text-lg">
+                <p className="font-victorMono text-sm md:text-base 2xl:text-lg">
                   Email: {personalData.email}
                 </p>
-                <p className="font-victorMono text-sm tracking-widest 2xl:text-lg">
+                <p className="font-victorMono text-sm md:text-base tracking-widest 2xl:text-lg">
                   Phone: {personalData.phone}
                 </p>
               </div>
