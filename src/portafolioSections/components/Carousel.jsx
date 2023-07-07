05715dabae86ac7f2c2298efab9995de63db8085
Leonardo <leonardo38361@gmail.com>
2023-07-07 05:07:14 -0300

    eliminación del react fragment

--- src/portafolioSections/components/Carousel.jsx
+++ src/portafolioSections/components/Carousel.jsx
@@ -32,45 +32,43 @@ export const Carousel = ({ proyects, darkMode }) => {
       }}
       pagination={{ clickable: true, el: `.${paginationClass}` }}
       scrollbar={{ draggable: true }}
-      className="mySwiper flex xs:w-64 w-72 flex-col sm:w-96 md:mx-10 md:w-3/5">
+      className="mySwiper flex w-72 flex-col xs:w-64 sm:w-96 md:mx-10 md:w-3/5">
       {proyects?.map((proyect) => {
         return (
-          <>
-            <SwiperSlide className="mb-10 flex justify-center" key={proyect.id}>
-              <div className="swiper-button image-swiper-button-next flex h-6 w-6 items-center justify-center rounded-full bg-cyan-400 transition-all duration-500 hover:opacity-50 md:h-10 md:w-10">
-                <span className="material-symbols-outlined text-base text-white md:text-xl xl:text-2xl">
-                  arrow_forward_ios
-                </span>
+          <SwiperSlide className="mb-10 flex justify-center" key={proyect.id}>
+            <div className="swiper-button image-swiper-button-next flex h-6 w-6 items-center justify-center rounded-full bg-cyan-400 transition-all duration-500 hover:opacity-50 md:h-10 md:w-10">
+              <span className="material-symbols-outlined text-base text-white md:text-xl xl:text-2xl">
+                arrow_forward_ios
+              </span>
+            </div>
+            <div className="swiper-button image-swiper-button-prev flex h-6 w-6 items-center justify-center rounded-full bg-cyan-400 transition-all duration-500 hover:opacity-50 md:h-10 md:w-10">
+              <span className="material-symbols-outlined text-base text-white md:text-xl xl:text-2xl">
+                arrow_back_ios_new
+              </span>
+            </div>
+            <img
+              className="z-0 w-11/12 rounded-md bg-neutral-200 p-2 shadow-lg dark:bg-neutral-400 2xl:rounded-lg"
+              src={proyect.image}
+              alt={proyect.title}
+            />
+            <div className="absolute -bottom-2 right-2 z-20 mr-2 flex animate-pulse flex-col place-items-center rounded-md bg-neutral-900 p-1 font-Kanit text-xs text-white transition delay-1000 dark:bg-neutral-600">
+              <div className="flex items-center justify-center">
+                <img
+                  className="w-6 p-1 md:w-10"
+                  src={proyect.technology}
+                  alt={proyect.title}
+                />
+                <a
+                  href={proyect.link}
+                  target="_blank"
+                  className="flex cursor-pointer p-1">
+                  <span className="material-symbols-outlined text-sm md:text-base xl:text-2xl">
+                    link
+                  </span>
+                </a>
               </div>
-              <div className="swiper-button image-swiper-button-prev flex h-6 w-6 items-center justify-center rounded-full bg-cyan-400 transition-all duration-500 hover:opacity-50 md:h-10 md:w-10">
-                <span className="material-symbols-outlined text-base text-white md:text-xl xl:text-2xl">
-                  arrow_back_ios_new
-                </span>
-              </div>
-              <img
-                className="z-0 w-11/12 rounded-md bg-neutral-200 p-2 shadow-lg dark:bg-neutral-400 2xl:rounded-lg"
-                src={proyect.image}
-                alt={proyect.title}
-              />
-              <div className="absolute -bottom-2 right-2 z-20 mr-2 flex animate-pulse flex-col place-items-center rounded-md bg-neutral-900 p-1 font-Kanit text-xs text-white transition delay-1000 dark:bg-neutral-600">
-                <div className="flex items-center justify-center">
-                  <img
-                    className="w-6 p-1 md:w-10"
-                    src={proyect.technology}
-                    alt={proyect.title}
-                  />
-                  <a
-                    href={proyect.link}
-                    target="_blank"
-                    className="flex cursor-pointer p-1">
-                    <span className="material-symbols-outlined text-sm md:text-base xl:text-2xl">
-                      link
-                    </span>
-                  </a>
-                </div>
-              </div>
-            </SwiperSlide>
-          </>
+            </div>
+          </SwiperSlide>
         );
       })}
       <div className={paginationClass}></div>
