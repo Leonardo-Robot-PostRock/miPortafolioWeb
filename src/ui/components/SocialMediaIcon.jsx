6f4cbb673279d661b6be72bb10b682d2f4c72759
Leonardo <leonardo38361@gmail.com>
2023-07-11 01:55:27 -0300

    nuevo componente reutilizable

---
+++ src/ui/components/SocialMediaIcon.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const SocialMediaIcon = ({ link, icon: Icon, alt, className }) => {
+  return (
+    <div className="flex h-max animate-pulse items-center rounded-lg border-2 border-solid border-sky-400 dark:border-white">
+      <a href={link}>
+        <Icon
+          className={`cursor-pointer fill-sky-400 p-1 dark:fill-white ${
+            className === 'home' && 'h-10 w-10 2xl:h-16 2xl:w-16'
+          } h-10 w-10`}
+          alt={alt}
+        />
+      </a>
+    </div>
+  );
+};
