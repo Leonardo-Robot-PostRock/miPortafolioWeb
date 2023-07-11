7d938c3fad416e048bfde4aa38e94975994820c0
Leonardo <leonardo38361@gmail.com>
2023-07-11 01:50:27 -0300

    ajustes de responsividad

--- src/portafolioSections/sections/Contact.jsx
+++ src/portafolioSections/sections/Contact.jsx
@@ -30,10 +30,10 @@ export const Contact = () => {
 
   return (
     <main
-      className="min-w-800 mb-3 h-screen xs:h-max 2xl:mb-20 xl:h-max"
+      className="min-w-800 mb-3 h-screen xs:h-max 2xl:mb-10 xl:h-max"
       id="contact">
       <section className="mt-24 h-full min-w-280 text-center md:mx-2">
-        <h1 className="font-Raleway text-5xl 2xl:mb-20">Contact</h1>
+        <h1 className="font-Raleway text-5xl 2xl:mb-10">Contact</h1>
         <div className="mx-2 my-5 2xl:mt-10 flex flex-col place-items-center xl:h-full">
           <div className="grid grid-cols-1 mb-4 md:mb-0 gap-6 xs:grid-cols-[repeat(1,250px)] 375:grid-cols-[repeat(1,300px)] sm:grid-cols-[repeat(1,330px)] md:grid-cols-[repeat(2,330px)] md:gap-20 xl:grid-cols-[repeat(2,400px)] 2xl:grid-cols-[repeat(2,minmax(470px,_300px))] 2xl:gap-32">
             {/* box 1 */}
@@ -53,7 +53,7 @@ export const Contact = () => {
                 <div className="flex flex-col">
                   <label className="font-Kanit xl:mb-2 2xl:mb-5 xl:text-lg">Message</label>
                   <textarea
-                    className="resize-none 2xl:h-64 rounded-sm border-2 border-solid border-neutral-200 p-2 text-neutral-950 outline-none dark:border-none dark:bg-neutral-800 dark:text-white"
+                    className="resize-none 2xl:h-56 rounded-sm border-2 border-solid border-neutral-200 p-2 text-neutral-950 outline-none dark:border-none dark:bg-neutral-800 dark:text-white"
                     rows="6"
                     name="message"
                   />
